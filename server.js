5e555f2d977fc8fd1a23544aa55ab9cddd74631a
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2026-04-08 20:20:01 +0530

    as

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app= require("./app")
 const Db=require("./database/db")
 Db()
 
-const Port=3000
+const Port=process.env.PORT || 5000
 
 app.listen(Port,()=>{
     console.log("Server is running")
